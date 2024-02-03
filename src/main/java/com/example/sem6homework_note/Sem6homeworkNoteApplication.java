1b6a336796ec763260fe179e8a5764279b038bd0
NikNik <56563372+Nikkru@users.noreply.github.com>
2024-02-03 16:18:12 +0300

    init

---
+++ src/main/java/com/example/sem6homework_note/Sem6homeworkNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.sem6homework_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem6homeworkNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem6homeworkNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sem6homework_note/Sem6homeworkNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sem6homework_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem6homeworkNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
